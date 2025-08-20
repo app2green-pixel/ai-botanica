8027c53443a958074c9c1af44b6c28c2862f2969
app2green-pixel <app2.green@gmail.com>
2025-08-20 16:01:33 +0200

    Update botanica_ai.py

--- botanica_ai.py
+++ botanica_ai.py
@@ -333,6 +333,5 @@ def add_info():
     return jsonify({"successo": f"Informazioni su '{sottoargomento}' aggiunte correttamente"}), 200
 
 if __name__ == '__main__':
-    port =
-int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
